00298c342a62fc8ed8af29fe0f24eb7d52afce80
harshitachhabria18 <harshitachhabria18@gmail.com>
2025-07-14 18:16:04 +0530

    Update app.py

--- Educational Chatbot/app.py
+++ Educational Chatbot/app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key_here"  # Replace with a strong secret key in production
+app.secret_key = "your_secret_key_here"  
 
 # Configure Gemini API Key
 genai.configure(api_key="your_api_key")
